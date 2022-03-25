65a26c30d1b7c2caeb916e3ce705e4f12ef6b61b
Anjali <apoolakkal@suyati.com>
2022-03-25 16:27:22 +0530

    Add project files.

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,30 @@
+﻿using Member.Data.Interface;
+using Member.Data.Model;
+using Member.Data.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : Controller
+    {
+        private IMember members = new MembersRepository();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Members>> GetAllMembers()
+        {
+            return members.GetAllMember();
+        }
+        [HttpGet]
+        public ActionResult<Members> GetMemberById(int id)
+        {
+            return members.GetMember(id);
+        }
+    }
+}
